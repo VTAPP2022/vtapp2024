ce7cf7a98e98315bc892ad281325fbf006547bf8
Akhil <akhilesh.tulluri@gmail.com>
2022-11-26 22:58:16 +0530

    feat(events): add filters and search

--- src/components/EventCard.js
+++ src/components/EventCard.js
@@ -2,19 +2,14 @@ import React from "react";
 
 function EventCard({ imgUrl, EventName, EventDisc, Organizer, Price, Type }) {
   return (
-    <div>
+    <div className="flex">
       <div className="max-w-xs rounded overflow-hidden shadow-lg bg-gray-800 m-4">
         <img className="w-full" src={imgUrl} alt={EventName} />
         <div className="px-6 py-4">
           <div className="text-white font-bold text-xl mb-2">{EventName}</div>
           <p className="text-gray-300 text-base">{EventDisc}</p>
-          {/* <br />
-
-          <button className="rounded-md p-3 px-7 bg-green-400 text-black">
-            ADD
-          </button> */}
         </div>
-        <div class="px-6 pt-4 pb-2">
+        <div class="px-6 pt-4">
           <span class="inline-block bg-gray-100 px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2 rounded-full">
             {Organizer}
           </span>
@@ -24,6 +19,15 @@ function EventCard({ imgUrl, EventName, EventDisc, Organizer, Price, Type }) {
           {/* <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
             #winter
           </span> */}
+        </div>
+        <br />
+        <div className="px-6 pb-4">
+          <button className="rounded-md p-2 bg-green-400 text-black mr-3">
+            Read more
+          </button>
+          <button className="rounded-md p-2 bg-green-400 text-black">
+            Register now
+          </button>
         </div>
       </div>
     </div>
--- src/pages/Event.js
+++ src/pages/Event.js
@@ -1,8 +1,16 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import EventCard from "../components/EventCard";
 import EventsList from "../data/events_list.json";
 
 function Event() {
+  const [filter, setFilter] = useState("");
+  const [search, setSearch] = useState("");
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    setEvents(EventsList);
+  }, []);
+
   return (
     <div>
       <section className="bg-slate-900 flex flex-col">
@@ -17,17 +25,98 @@ function Event() {
             <span className="inline-block w-1 h-1 bg-blue-500 rounded-full"></span>
           </div>
 
+          <div className="flex justify-center mt-5">
+            <div className="w-full md:w-2/3 rounded shadow p-5 bg-gray-900">
+              <div className="relative">
+                <div className="absolute flex items-center ml-2 h-full">
+                  <svg
+                    className="w-4 h-4 fill-current text-primary-gray-dark"
+                    viewBox="0 0 16 16"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d="M15.8898 15.0493L11.8588 11.0182C11.7869 10.9463 11.6932 10.9088 11.5932 10.9088H11.2713C12.3431 9.74952 12.9994 8.20272 12.9994 6.49968C12.9994 2.90923 10.0901 0 6.49968 0C2.90923 0 0 2.90923 0 6.49968C0 10.0901 2.90923 12.9994 6.49968 12.9994C8.20272 12.9994 9.74952 12.3431 10.9088 11.2744V11.5932C10.9088 11.6932 10.9495 11.7869 11.0182 11.8588L15.0493 15.8898C15.1961 16.0367 15.4336 16.0367 15.5805 15.8898L15.8898 15.5805C16.0367 15.4336 16.0367 15.1961 15.8898 15.0493ZM6.49968 11.9994C3.45921 11.9994 0.999951 9.54016 0.999951 6.49968C0.999951 3.45921 3.45921 0.999951 6.49968 0.999951C9.54016 0.999951 11.9994 3.45921 11.9994 6.49968C11.9994 9.54016 9.54016 11.9994 6.49968 11.9994Z"></path>
+                  </svg>
+                </div>
+
+                <input
+                  type="text"
+                  onChange={(e) => setSearch(e.target.value)}
+                  value={search}
+                  placeholder="Search for an event..."
+                  class="px-8 py-3 w-full rounded-md bg-gray-100 text-sm text-gray-700 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0"
+                />
+              </div>
+
+              <div className="flex items-center justify-between mt-4">
+                <p className="font-medium">Filters</p>
+
+                <button
+                  className="px-4 py-2 bg-gray-100 hover:bg-gray-200 text-gray-800 text-sm font-medium rounded-md"
+                  onClick={() => setFilter("")}
+                >
+                  Reset Filter
+                </button>
+              </div>
+
+              <div>
+                <div className="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4 mt-4">
+                  {/* <select className="px-4 py-3 w-full rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0 text-sm text-gray-700">
+                    <option value="">All Organisers</option>
+                    <option value="">For Rent</option>
+                    <option value="for-sale">For Sale</option>
+                  </select> */}
+
+                  <select
+                    className="px-4 py-3 w-full rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0 text-sm text-gray-700"
+                    value={filter}
+                    onChange={(e) => setFilter(e.target.value)}
+                  >
+                    <option value="">All Event Types</option>
+                    <option value="codeathon">Codeathon</option>
+                    <option value="competition">Competition</option>
+                    <option value="exhibition">Exhibition</option>
+                    <option value="game">Game</option>
+                    <option value="workshop">Workshop</option>
+                  </select>
+                </div>
+              </div>
+            </div>
+          </div>
+
           <div className="flex flex-wrap justify-center mt-6">
-            {EventsList.map((e) => (
-              <EventCard
-                EventName={e.event_name}
-                EventDisc={e.description}
-                Organizer={e.organiser}
-                Type={e.event_type}
-                Price="20000"
-                imgUrl="https://expertus.ee/wp-content/uploads/2019/02/placeholder-16.9.jpg"
-              />
-            ))}
+            {events.map((e) => {
+              if (filter !== "") {
+                if (
+                  e.event_type.toLowerCase().trim() !== filter.toLowerCase()
+                ) {
+                  // TODO: Change this?
+                  return;
+                }
+              }
+
+              if (search !== "") {
+                if (
+                  !e.event_name.toLowerCase().includes(search) &&
+                  !e.description.toLowerCase().includes(search) &&
+                  !e.organiser.toLowerCase().includes(search) &&
+                  !e.event_type.toLowerCase().includes(search)
+                ) {
+                  // TODO: Change this?
+                  return;
+                }
+              }
+              return (
+                <EventCard
+                  EventName={e.event_name}
+                  EventDisc={e.description}
+                  Organizer={e.organiser}
+                  Type={e.event_type}
+                  Price="20000"
+                  imgUrl="https://expertus.ee/wp-content/uploads/2019/02/placeholder-16.9.jpg"
+                />
+              );
+            })}
           </div>
         </div>
       </section>
