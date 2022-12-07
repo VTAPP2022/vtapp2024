646fce167ebd2c6305d1ed247e285cf2150bf14b
saipranay47 <saipranayadepu07@gmail.com>
2022-12-07 12:10:10 +0530

    support team

--- src/pages/Team.js
+++ src/pages/Team.js
@@ -145,6 +145,55 @@ function Team() {
           />
         </div>
       </div>
+      <div className="container px-6 py-10 mx-auto ">
+        <h1 className="text-3xl font-semibold text-center capitalize lg:text-4xl text-white">
+          Support Team
+        </h1>
+
+        <div className="flex justify-center mx-auto mt-2">
+          <span className="inline-block w-40 h-1 bg-green-500 rounded-full"></span>
+          <span className="inline-block w-3 h-1 mx-1 bg-green-500 rounded-full"></span>
+          <span className="inline-block w-1 h-1 bg-green-500 rounded-full"></span>
+        </div>
+
+        <div className="container px-6 py-10 mx-auto ">
+          <div className="flex flex-wrap justify-center mt-6">
+            <TeamCard
+              name="Sai kiran"
+              imageUrl="https://i.ibb.co/W0Fmk85/Whats-App-Image-2022-12-06-at-5-12-29-PM-1.jpg"
+              designation="Chief Financial Officer"
+            />
+
+            <TeamCard
+              name="A. Sekhar"
+              imageUrl="https://i.ibb.co/fMMZdgQ/Screenshot-2022-12-07-at-11-22-32-AM.png"
+              designation="Accountant"
+            />
+
+            <TeamCard
+              name="Chandrasekar"
+              imageUrl="https://i.ibb.co/YWJmVpj/Whats-App-Image-2022-12-06-at-5-37-01-PM-1.jpg"
+              designation="purchase officer"
+            />
+
+            <TeamCard
+              name="Haritha P"
+              imageUrl="https://st3.depositphotos.com/6672868/13701/v/600/depositphotos_137014128-stock-illustration-user-profile-icon.jpg"
+              designation="Senior Manager, HR"
+            />
+            <TeamCard
+              name="Dr. E. Ajith Jubilson"
+              imageUrl="https://vitap.ac.in/wp-content/uploads/2019/06/Mr.-E.-AJITH-JUBILSON.jpg"
+              designation=""
+            />
+            <TeamCard
+              name="Dr. R. Nandha Kumar"
+              imageUrl="https://vitap.ac.in/wp-content/uploads/2020/08/Nandha_70079.jpg"
+              designation=""
+            />
+          </div>
+        </div>
+      </div>
     </section>
   );
 }
