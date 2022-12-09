5361ac4b0d013c8fc8550421ae9f702de7b34a91
saipranay47 <saipranayadepu07@gmail.com>
2022-12-09 21:31:45 +0530

    updated bus details

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import About from "../components/About";
 import EventView from "../components/EventView";
+import HeaderInfo from "../components/HeaderInfo";
 import Hero from "../components/Hero";
 import TeamAndSponsors from "../components/TeamAndSponsors";
 import TShirt from "../components/TShirt";
@@ -9,6 +10,7 @@ function Home({ events }) {
   return (
     <div>
       <Hero />
+      <HeaderInfo/>
       <About />
       <EventView events={events} />
       <TeamAndSponsors />
