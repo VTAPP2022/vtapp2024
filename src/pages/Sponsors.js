2b76d2aa45ebf3567e8ae0519133b2279a62a262
saipranay47 <saipranayadepu07@gmail.com>
2023-11-24 18:58:59 +0530

    update sponsors

--- src/pages/Sponsors.js
+++ src/pages/Sponsors.js
@@ -5,11 +5,11 @@ function Card({ imgurl, white }) {
     <div>
       <div
         className={`h-[200px] w-[350px] overflow-hidden text-center m-[35px] rounded-xl ${
-          white == true ? `bg-gray-800` : `bg-white`
+          white == true ? `bg-white` : `bg-gray-900`
         }`}
       >
         <div className="h-full w-full flex justify-center align-center">
-          {/* <img className="p-2 m-auto w-9/12 max-w-xs h-auto" src={imgurl} /> */}
+          <img className="p-2 m-auto w-9/12 max-w-xs h-auto" src={imgurl} />
         </div>
       </div>
     </div>
@@ -30,19 +30,8 @@ function Sponsors() {
         <br />
         <div className="p-2.5">
           <div className="flex flex-wrap justify-center align-center">
-            {/* {Sponsor.map((card) => (
-              <Card imgurl={card.imgurl} white = {card.blackBG}/>
-            ))} */}
-
-            {/* first map 2 image cards then map al other */}
-
-            {Sponsor.slice(0, 2).map((card) => (
-              <Card imgurl={card.imgurl} white={card.blackBG} />
-            ))}
-          </div>
-          <div className="flex flex-wrap justify-center align-center">
-            {Sponsor.slice(2, Sponsor.length).map((card) => (
-              <Card imgurl={card.imgurl} white={card.blackBG} />
+            {Sponsor.slice(0, Sponsor.length).map((card) => (
+              <Card imgurl={card.imgurl} white={card.whiteBG} />
             ))}
           </div>
         </div>
