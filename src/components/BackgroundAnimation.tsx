74845c645dd4048de1ef2a85b3ec492c47ef9fba
Akhil <akhilesh.tulluri@gmail.com>
2023-12-04 13:02:34 +0530

    chore(animation): change starfield animation position

--- src/components/BackgroundAnimation.tsx
+++ src/components/BackgroundAnimation.tsx
@@ -6,7 +6,7 @@ function BackgroundAnimation() {
   return (
     <StarfieldAnimation
       style={{
-        position: "fixed",
+        position: "absolute",
         width: "100%",
         height: "100%",
       }}
