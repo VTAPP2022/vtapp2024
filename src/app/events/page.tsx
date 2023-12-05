1f87e91ebbeb502e5ec63420701643c840b42a4a
Akhil <akhilesh.tulluri@gmail.com>
2023-12-05 14:23:01 +0530

    fix(events): use fetchEventsFromAirtable from @vtapp/lib

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,56 +1,11 @@
 import EventList from "@vtapp/components/EventList";
+import { fetchEventsFromAirtable } from "@vtapp/lib/events";
 import { AirtableEventResponse } from "@vtapp/types";
 
 export const runtime = "edge";
 
-async function getData() {
-  const requiredFields = [
-    "event_id",
-    "event_name",
-    "event_type",
-    "price",
-    "organiser",
-    "description",
-    "slug",
-    "poster_url",
-    "datetime_start",
-    "datetime_end",
-    "place",
-    "floor",
-    "room",
-  ];
-
-  // make a string of required fields (fields[]=event_id&fields[]=event_name&...)
-  const requiredFieldsString = requiredFields
-    .map((field) => `fields[]=${field}`)
-    .join("&");
-  const AIRTABLE_EVENTS_ENDPOINT = encodeURI(
-    `${process.env.AIRTABLE_BASE_URL}/${process.env.AIRTABLE_BASE_ID}/events?view=Grid view&${requiredFieldsString}`
-  );
-
-  const cacheOptions =
-    process.env.NODE_ENV === "development"
-      ? {
-          next: { revalidate: 60 },
-        }
-      : { cache: "no-store" as RequestCache };
-
-  const response = await fetch(AIRTABLE_EVENTS_ENDPOINT, {
-    headers: {
-      Authorization: `Bearer ${process.env.AIRTABLE_PERSONAL_ACCESS_TOKEN}`,
-    },
-    ...cacheOptions,
-  });
-
-  const data = (await response.json()) as AirtableEventResponse;
-
-  const events = data.records.map((record) => record.fields);
-
-  return events;
-}
-
 export default async function Events() {
-  const eventsDataFromAirtable = await getData();
+  const eventsDataFromAirtable = await fetchEventsFromAirtable();
 
   return <EventList events={eventsDataFromAirtable} />;
 
