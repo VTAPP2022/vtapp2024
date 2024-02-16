8c8fe10af25e617e522e983b09c6db412a91b5ac
Akhil <akhilesh.tulluri@gmail.com>
2024-02-16 23:09:01 +0530

    fix(workers): cache issue w/ planetscale

--- src/db/index.ts
+++ src/db/index.ts
@@ -7,7 +7,13 @@ export const config = {
   password: process.env.DATABASE_PASSWORD,
 };
 
-const connection = connect(config);
+const connection = connect({
+  ...config,
+  fetch: (url, init) => {
+    delete (init as any)["cache"]; // Remove cache header
+    return fetch(url, init);
+  },
+});
 
 export const db = drizzle(connection);
 export type DbClient = typeof db;
