3cc4b0b3f3729737f0ead2abe03e92703d991551
Akhil <akhilesh.tulluri@gmail.com>
2024-02-16 14:24:57 +0530

    feat(schedule): add schedule page w/ airtable

--- src/app/schedule/page.tsx
+++ src/app/schedule/page.tsx
@@ -1,14 +1,25 @@
+import { AirtableEventsEmbed } from "@vtapp/components/AirtableEventsEmbed";
+
 function Schedule() {
   return (
-    <div>
-      <section className="bg-slate-900 flex flex-col min-h-screen justify-center items-center">
-        <div className=" px-6 py-10 mx-auto ">
+    <>
+      <section className="bg-slate-900 flex flex-col">
+        <div className="container px-6 py-10 mx-auto ">
           <h1 className="text-3xl font-semibold text-center capitalize lg:text-4xl text-white">
-            Schedule will be displayed soon..
+            Schedule
           </h1>
+          <div className="flex justify-center mx-auto mt-2">
+            <span className="inline-block w-40 h-1 bg-blue-500 rounded-full"></span>
+            <span className="inline-block w-3 h-1 mx-1 bg-blue-500 rounded-full"></span>
+            <span className="inline-block w-1 h-1 bg-blue-500 rounded-full"></span>
+          </div>
+
+          <div className="flex flex-wrap justify-center mt-6">
+            <AirtableEventsEmbed />
+          </div>
         </div>
       </section>
-    </div>
+    </>
   );
 }
 
---
+++ src/components/AirtableEventsEmbed.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+export const AirtableEventsEmbed = () => (
+  <iframe
+    className="airtable-embed z-10"
+    src="https://airtable.com/embed/appHd1vGWOCIRWzrv/shrc2ubsyqnySxGzS?backgroundColor=blue&viewControls=on"
+    width="100%"
+    height={800}
+    style={{ background: "transparent", border: "1px solid #ccc" }}
+  ></iframe>
+);
